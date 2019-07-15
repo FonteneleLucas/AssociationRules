f64349affb9d4f1b799e49ba8d3403b89fd07f87
Lucas Fontenele <Lucas Fontenele>
2019-07-15 03:39:23 -0300

    adicionando resultados Iris e Lenses

--- machineLearning.py
+++ machineLearning.py
@@ -1,6 +1,7 @@
 from efficient_apriori import apriori
 
 columns = ["SP_Length: ", "SP_Width: ", "PT_Legth: ", "PT_Width: ", "Class: "]
+# columns = ["ID: ", "idade: ", "disturbios_visao: ", "astigmatico: ", "taxa_lagrimas: ", "Classe: "]
 
 import csv
 
@@ -8,7 +9,10 @@ f = open('iris.csv', 'r').read()
 
 dataList = [line.split(',') for line in f.split('\n') if line]
 
+# print(dataList)
+
 items = []
+
 
 for i in range(len(dataList)):
     item = []
@@ -17,9 +21,11 @@ for i in range(len(dataList)):
 
     items.append(item)
 
-itemsets, rules = apriori(items, min_support=0.1, min_confidence=0.2)
+# print(items)
 
-rules_rhs = filter(lambda rule: len(rule.lhs) > 0 and len(rule.rhs) > 0 , rules)
+itemsets, rules = apriori(items, min_support=0.0, min_confidence=0.01)
+
+rules_rhs = filter(lambda rule: len(rule.lhs) <= 2 and len(rule.rhs) == 2 , rules)
 for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
   print(rule)
 
@@ -30,5 +36,5 @@ for rule in sorted(rules_rhs, key=lambda rule: rule.lift):
     2) Realizar a extração de Regras de Associação de cada um dos datasets de 3 formas
         2.1) Todas as regras possíveis com minsup=0.1 e minconf=0.2
         2.2) Regras de tamanho 2, ou seja 1 elemento no antecedente e 1 no consequente da regra, com minsup=0.0 e minconf=0.01
-        2.3) Regras com antecedente de tamanho 1 ou 2 é consequente de tamanho 2, com minsup=0.0 e minconf=0.01
+        2.3) Regras com antecedente de tamanho 1 ou 2 e consequente de tamanho 2, com minsup=0.0 e minconf=0.01
 """
